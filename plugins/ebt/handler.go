66fb73c22cb4a1ceee184593d9141bb57b88f49b
Matthew Lorentz <matt@lorentz.is>
2022-09-29 10:52:00 -0400

    Fix EBT replication to not skip replication of your own feed when its length is 0

--- plugins/ebt/handler.go
+++ plugins/ebt/handler.go
@@ -115,7 +115,7 @@ func (h *MUXRPCHandler) sendState(ctx context.Context, tx *muxrpc.ByteSink, remo
 	selfRef := h.self.String()
 
 	// don't receive your own feed
-	if myNote, has := currState[selfRef]; has {
+	if myNote, has := currState[selfRef]; has && myNote.Seq > 0 {
 		myNote.Receive = false
 		currState[selfRef] = myNote
 	}
